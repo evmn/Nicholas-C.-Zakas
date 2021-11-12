f3aa46dfb5ff65fa83a991750679135399b35d3c
evmn <38780274+evmn@users.noreply.github.com>
2021-11-12 16:30:33 +0800

    convert absolute url(folder included) to local absolute url

--- scripts/absolute_to_local.sh
+++ scripts/absolute_to_local.sh
@@ -8,7 +8,7 @@ for html in $(find . -type f -name index.html)
 do
     if [  $(echo "$html" | grep "index.html") ];then
         folder=$(echo "$html" | rev | cut -d "/" -f 2- | rev)
-        cat $html |  grep -Po "https://humanwhocodes.com/blog/20.*?(index.html)" | rev | cut -d "/" -f 2- | rev | sort | uniq |  while read -r url ; do
+        cat $html |  grep -Po "https://humanwhocodes.com/blog/20[0-9/]{5}/.*?/" | rev | cut -d "/" -f 2- | rev | sort | uniq |  while read -r url ; do
             absolute=$(echo "$url"  | cut -d "/" -f 5- )
             before=$(echo "https://humanwhocodes.com/blog/$absolute" | sed 's/\//\\\//g')
             after=$(echo "http://127.0.0.1:8000/blog/$absolute" | sed 's/\//\\\//g')
